e4089628289848118d6e71c8c335dd9f7fa23f9f
Ganesh R <105833610+GaneshR24@users.noreply.github.com>
2022-08-27 22:11:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Show menu
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        });
+    }
+}
+
+showMenu('nav_toggle','nav_menubar')
+
+// Active
+
+const navLink = document.querySelectorAll('.nav_link')
+navLink.forEach(n=> n.classList.remove('active'))
+
+function linkAction() {
+     navLink.forEach(n=> n.classList.remove('active'))
+    this.classList.add('active')
+
+     const navMenubar = document.getElementById('nav_menubar') 
+     navMenubar.classList.remove('show')
+}
+
+navLink.forEach(n =>n.addEventListener('click', linkAction))
